a76803367a06cc58a6bf8dd0c8ee9751ee85a649
Carlos <carlosgilino@gmail.com>
2025-08-04 22:02:13 +0200

    create header

---
+++ frontend/src/components/header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Logo from './Logo';
+import NavMenu from './NavMenu';
+import Button from '../ui/Button';
+
+export default function Header() {
+    return (
+        <div className='flex justify-between items-center px-5 h-16 bg-gray-600 text-white flex-shrink-0'>
+            <Logo />
+            <NavMenu />
+            <Button />
+        </div>
+    )
+}
