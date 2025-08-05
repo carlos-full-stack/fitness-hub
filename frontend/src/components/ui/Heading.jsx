ca5490cb02a097ae8baf47df44692a957a2908f9
Carlos <carlosgilino@gmail.com>
2025-08-05 14:25:56 +0200

    create Heading component

--- frontend/src/components/ui/Heading.jsx
+++ frontend/src/components/ui/Heading.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
-export default function Heading() {
+
+
+export default function Heading({ level = 'h1', headingText, eyebrowText = false, subHeadingText }) {
+
+
     return (
-        <div>Heading</div>
+
+        <div className='flex flex-col'>
+            {eyebrowText && <span>{eyebrowText}</span>}
+            {level === 'h1' ? <h1> {headingText}</h1> : <h2> {headingText}</h2>}
+            {subHeadingText && <p>{eyebrowText}</p>}
+        </div>
+
     )
 }
