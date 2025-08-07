63adab7b453c152b3ac150e24e1462498db1583e
Carlos <carlosgilino@gmail.com>
2025-08-07 13:51:10 +0200

    improve heading style on hero

--- frontend/src/components/body/Hero/index.jsx
+++ frontend/src/components/body/Hero/index.jsx
@@ -5,7 +5,7 @@ import RightColumn from './RightColumn';
 export default function Hero() {
     return (
         <div className='flex flex-col gap-6 lg:gap-0 lg:flex-row bg-gray-800 pt-16'>
-            <div className='lg:w-1/2 text-white px-16'><LeftColumn /></div>
+            <div className='lg:w-1/2 text-white pl-16'><LeftColumn /></div>
             <div className='lg:w-1/2'><RightColumn /></div>
         </div>
     )
--- frontend/src/components/ui/Heading.jsx
+++ frontend/src/components/ui/Heading.jsx
@@ -9,7 +9,7 @@ export default function Heading({ level = 'h1', headingText, eyebrowText, subHea
 
         <div className='flex flex-col'>
             {eyebrowText && <span className='text-primary text-sm'>{eyebrowText}</span>}
-            {level === 'h1' ? <h1 className=' text-5xl md:text-6xl uppercase'> {headingText}</h1> : <h2> {headingText}</h2>}
+            {level === 'h1' ? <h1 className=' text-6xl md:text-8xl uppercase font-Druk tracking-wider leading-20'> {headingText}</h1> : <h2> {headingText}</h2>}
             {subHeadingText && <p className='pt-6'>{subHeadingText}</p>}
         </div>
 
