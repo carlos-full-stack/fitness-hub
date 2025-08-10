e71aa47278b8f4414a4a023ce9295ac5b54c544e
Carlos <carlosgilino@gmail.com>
2025-08-10 19:46:58 +0200

    improve Hero styles

--- frontend/src/components/body/Hero/index.jsx
+++ frontend/src/components/body/Hero/index.jsx
@@ -3,10 +3,10 @@ import LeftColumn from './LeftColumn';
 import RightColumn from './RightColumn';
 
 export default function Hero() {
-    return ( 
-        <div className='flex flex-col gap-6 lg:gap-0 lg:flex-row bg-gray-800 pt-16'>
-            <div className='lg:w-1/2 text-white pl-16'><LeftColumn /></div>
-            <div className='lg:w-1/2'><RightColumn /></div>
+    return (
+        <div className='flex flex-col gap-6 lg:gap-0 lg:flex-row bg-gray-800 px-8 pt-8  md:px-16 md:pt-16'>
+            <div className='lg:w-[60%] text-white'><LeftColumn /></div>
+            <div className='lg:w-[40%] flex flex-col justify-end'><RightColumn /></div>
         </div>
     )
 }
