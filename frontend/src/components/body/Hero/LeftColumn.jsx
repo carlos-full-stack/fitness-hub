44d5d913ef20c5a2b1795c3b12b022ed90cb0b21
Carlos <carlosgilino@gmail.com>
2025-08-10 19:48:37 +0200

    LeftColumn code improvement

--- frontend/src/components/body/Hero/LeftColumn.jsx
+++ frontend/src/components/body/Hero/LeftColumn.jsx
@@ -5,18 +5,19 @@ import Statistics from '../../ui/Statistics';
 
 export default function LeftColumn() {
 
-    const headingText = <div className='pt-2'><div><span>Stronger</span><span className='text-stroke pl-2'>every day</span></div><div className='pt-2'><span>Fitter</span><span className='text-stroke pl-2'>forever</span></div></div>;
-    const eyebrowText = 'Sweat today. Shine torrorrow';
-    const subHeadingText = 'Where fitness meets inpiration, and every drop of sweat tells a story of deterination.'
-
     return (
         <>
-            <Heading level='h1' headingText={headingText} eyebrowText={eyebrowText} subHeadingText={subHeadingText} />
+            <Heading
+                level='h1'
+                headingText={<div><div><span>Stronger</span><span className='text-stroke pl-2'>every day</span></div><div className='pt-2'><span>Fitter</span><span className='text-stroke pl-2'>forever</span></div></div>}
+                eyebrowText='Sweat today. Shine torrorrow'
+                subHeadingText='Where fitness meets inpiration, and every drop of sweat tells a story of deterination.'
+            />
             <div className="flex flex-row gap-3 pt-6">
                 <Button color='primary' text='Get Started' />
                 <Button color='secondary' text='How it works' />
             </div>
-            <div className='pt-12'>
+            <div className='py-9'>
                 <Statistics />
             </div>
         </>
