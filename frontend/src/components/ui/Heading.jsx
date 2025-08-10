74fc943cf16aa0fb08b59bb2e799c4046303b4b6
Carlos <carlosgilino@gmail.com>
2025-08-10 19:47:41 +0200

    improve Heading styles

--- frontend/src/components/ui/Heading.jsx
+++ frontend/src/components/ui/Heading.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 
 
 
-export default function Heading({ level = 'h1', headingText, eyebrowText, subHeadingText }) {
+export default function Heading({ level = 'h1', headingText, eyebrowText, subHeadingText = '', textAlign = 'left' }) {
 
+
+    const headingAlign = textAlign === 'left'
+        ? 'text-left'
+        : 'text-center';
 
     return (
 
-        <div className='flex flex-col'>
-            {eyebrowText && <span className='text-primary text-sm'>{eyebrowText}</span>}
-            {level === 'h1' ? <h1 className=' text-6xl md:text-8xl uppercase font-Druk tracking-wider leading-20'> {headingText}</h1> : <h2> {headingText}</h2>}
+        <div className={`${headingAlign} flex flex-col text-white`}>
+            {eyebrowText && <span className='text-primary text-sm py-2'>{eyebrowText}</span>}
+            {level === 'h1' ? <h1 className='text-5xl md:text-8xl lg:text-6xl xl:text-8xl uppercase font-Druk tracking-wider leading-10 md:leading-20 lg:leading-12 xl:leading-20'> {headingText}</h1> : <h2 className='text-2xl md:text-5xl uppercase'> {headingText}</h2>}
             {subHeadingText && <p className='pt-6'>{subHeadingText}</p>}
         </div>
 
