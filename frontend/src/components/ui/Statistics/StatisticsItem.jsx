51b8975a63b118a06e892b5f2c3fb5ec71cc2190
Carlos <carlosgilino@gmail.com>
2025-08-10 19:50:40 +0200

    Fix text on StatisticsItem

--- frontend/src/components/ui/Statistics/StatisticsItem.jsx
+++ frontend/src/components/ui/Statistics/StatisticsItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function StatisticsItem({ number, text }) {
     return (
         <div className='flex flex-col gap-2'>
-            {number && <span className='text-5xl text-stroke'>{number}</span>}
+            {number && <span className='text-3xl md:text-5xl lg:text-3xl xl:text-5xl text-stroke'>{number}</span>}
             {text && <span className='text-lg capitalize'>{text}</span>}
         </div>
     )
