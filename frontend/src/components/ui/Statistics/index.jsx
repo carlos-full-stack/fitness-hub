a36e35a46c4b59ef519f107be18f1477d9d01377
Carlos <carlosgilino@gmail.com>
2025-08-10 19:49:58 +0200

    improve Statistics responsiveness

--- frontend/src/components/ui/Statistics/index.jsx
+++ frontend/src/components/ui/Statistics/index.jsx
@@ -10,7 +10,7 @@ export default function Statistics() {
     }
 
     return (
-        <ul className='inline-flex gap-10'>
+        <ul className='inline-flex gap-7 md:gap-10'>
             {Object.entries(statistics).map(([text, number]) => (
                 <li key={text}>
                     <StatisticsItem text={text} number={number} />
