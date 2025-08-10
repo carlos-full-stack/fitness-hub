e74bf47ec193a944225104044fc4fd5419dcf63a
Carlos <carlosgilino@gmail.com>
2025-08-10 19:46:25 +0200

    complete Section component

--- frontend/src/components/ui/Section.jsx
+++ frontend/src/components/ui/Section.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-export default function Section() {
+export default function Section({ backgroundColor, children }) {
+
+
     return (
-        <div>Section</div>
+        <div className={`${backgroundColor === 'default' ? 'bg-gray-800' : 'bg-primary'} p-8 md:px-16 flex flex-col gap-3`}>
+            {children}
+        </div >
     )
 }
