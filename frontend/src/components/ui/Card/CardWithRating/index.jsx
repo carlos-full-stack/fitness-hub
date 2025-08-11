11fed2ee9228e519078d7a7a945cd0dc2418ad7f
Carlos <carlosgilino@gmail.com>
2025-08-11 19:17:09 +0200

    add new props to CardWithRating

--- frontend/src/components/ui/Card/CardWithRating/index.jsx
+++ frontend/src/components/ui/Card/CardWithRating/index.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Card from '..'
 import Rating from './Rating'
 
-export default function CardWithRating({ rating, feedback, userImgUrl }) {
-    const children = <div className='flex flex-col justify-between gap-2 p-6 h-full'>
+export default function CardWithRating({ rating, feedback, userImgUrl, userName, userRole }) {
+    const children = <div className='flex flex-col justify-between gap-2 p-7 h-full'>
         <div>
             <Rating stars={rating} />
             <p className='pt-3'>{feedback}</p>
         </div>
-        <img width={20} height={20} src={userImgUrl} />
+        <div className='flex w-full pl-1 gap-2'>
+            <img width={20} height={20} src={userImgUrl} />
+            <div className='flex flex-col gap-2'>
+                <span className='font-bold'>{userName}</span>
+                <span>{userRole}</span>
+            </div>
+        </div>
     </div>
 
     return (
