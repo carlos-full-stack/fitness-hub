10288718022c9939dba76273aea28e70a10a415b
Carlos <carlosgilino@gmail.com>
2025-08-11 19:18:19 +0200

    remove unnecesary div

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,9 @@ import Body from './components/body/'
 function App() {
 
   return (
-    <div className='w-full h-full bg-gray-100 py-10'>
-      <div className='flex flex-col h-full w-[350px] sm:w-[500px] md:w-[600px] lg:w-[900px] xl:w-[1200px] xxl:w-[1500px] mx-auto'>
-        <Header />
-        <Body />
-      </div>
+    <div className='flex flex-col h-full my-5 md:my-8 lg:my-12 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[900px] xl:w-[1200px] xxl:w-[1500px] mx-auto'>
+      <Header />
+      <Body />
     </div>
   )
 }
