8cecf9a3ce5ef898318fea2d0b0ae7a3f0ac6b47
Carlos <carlosgilino@gmail.com>
2025-08-11 10:48:42 +0200

    create CardWithPrice component

---
+++ frontend/src/components/ui/Card/CardWithPrice.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Card from '.'
+import Button from '../Button'
+
+export default function CardWithPrice({ price, title, description }) {
+
+    const children = <div className='flex flex-col justify-between gap-2 p-6 h-full'>
+        <div>
+            <span className='text-5xl'>{`${price}€`}</span>
+            <h3 className='text-xl pt-1'>{title}</h3>
+            <p className='pt-3'>{description}</p>
+        </div>
+        <Button text='Discover more' />
+    </div>
+
+    return (
+        <Card children={children} />
+    )
+}
