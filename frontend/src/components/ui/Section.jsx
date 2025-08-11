82802ad47ffe038b7640d548f9702ad5e1891365
Carlos <carlosgilino@gmail.com>
2025-08-11 10:51:18 +0200

    add more bg color options to Section component

--- frontend/src/components/ui/Section.jsx
+++ frontend/src/components/ui/Section.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-export default function Section({ backgroundColor, children }) {
+export default function Section({ backgroundColor = 'default', children }) {
 
+    const backgroundClasses = {
+        light: 'bg-gray-700',
+        dark: 'bg-gray-800',
+        default: 'bg-gray-800'
+    }
+
+    const bgColor = backgroundClasses[backgroundColor]
 
     return (
-        <div className={`${backgroundColor === 'default' ? 'bg-gray-800' : 'bg-primary'} p-8 md:px-16 flex flex-col gap-3`}>
+        <div className={`${bgColor} p-8 md:px-16 flex flex-col gap-3`}>
             {children}
         </div >
     )
