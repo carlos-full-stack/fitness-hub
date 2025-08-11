83d6943dcadc03d07652b317587cbd3d36745c6b
Carlos <carlosgilino@gmail.com>
2025-08-11 19:14:00 +0200

    add Button tyupe prop. Add new color options

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
-export default function Button({ text, color = 'primary' }) {
+export default function Button({ text, color = 'primary', type = 'button' }) {
 
     const baseClasses = 'capitalize py-2 px-6 rounded-sm';
 
-    const variantClasses = color === 'primary'
-        ? 'text-gray-800 bg-primary'
-        : 'text-white bg-transparent border border-white';
+    const buttonColors = {
+        primary: 'text-gray-800 bg-primary',
+        light: 'text-white bg-gray-700',
+        dark: 'text-white bg-gray-800',
+        transparent: 'text-white bg-transparent border border-white'
+    }
+
+    const variantClasses = buttonColors[color];
 
     return (
-        <button className={`${baseClasses} ${variantClasses}`}> {text}</ button >
+        <button type={type} className={`${baseClasses} ${variantClasses}`}> {text}</ button >
     )
 }
