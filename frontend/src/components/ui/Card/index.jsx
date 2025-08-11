6c1eaf001354b3c4e8d5da13cfd9075271873e20
Carlos <carlosgilino@gmail.com>
2025-08-11 10:52:50 +0200

    add more color options to Card

--- frontend/src/components/ui/Card/index.jsx
+++ frontend/src/components/ui/Card/index.jsx
@@ -2,14 +2,27 @@ import React, { useState } from 'react'
 import styles from './card.module.css'
 
 
-export default function Card({ bgUrl = 'none', children = null }) {
+export default function Card({ bgUrl = 'none', bgColor = 'default', children = null }) {
 
     const backgroundImage = bgUrl !== 'none'
         ? `url(${bgUrl})`
         : 'none';
 
+    const bgColorClasses = {
+        light: 'bg-gray-700',
+        dark: 'bg-gray-800',
+        primary: 'bg-primary',
+        default: 'bg-gray-800'
+    }
+
+    const CardBgColor = bgColorClasses[bgColor]
+
+    const textColor = bgColor !== 'primary'
+        ? 'text-white'
+        : 'text-black'
+
     return (
-        <div style={{ backgroundImage: ` ${backgroundImage}`, backgroundSize: 'cover' }} className={`${styles.cardBaseShape} h-[350px] relative`}>
+        <div style={{ backgroundImage: ` ${backgroundImage}`, backgroundSize: 'cover' }} className={`${styles.cardBaseShape} ${CardBgColor} ${textColor} h-[350px] relative`}>
             {children}
         </div >
     )
