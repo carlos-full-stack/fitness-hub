71baa8ddadfdf71682cf1989f02f438157388a36
Carlos <carlosgilino@gmail.com>
2025-08-12 22:48:32 +0200

    improve logo style

--- frontend/src/components/header/Logo.jsx
+++ frontend/src/components/header/Logo.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Logo() {
     return (
-        <div className='font-Druk text-5xl tracking-wide uppercase'><span>Fitness</span><span className='text-primary'>Hub</span></div>
+        <div className='font-Druk text-5xl text-center md:text-start tracking-wide uppercase'><span className='text-white'>Fitness</span><span className='text-primary'>Hub</span></div>
     )
 }
