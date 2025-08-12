53884a3bf7e163b210a0e9f5bd9b4439515ca65d
Carlos <carlosgilino@gmail.com>
2025-08-12 18:34:27 +0200

    create site logo

--- frontend/src/components/header/Logo.jsx
+++ frontend/src/components/header/Logo.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Logo() {
     return (
-        <div>Logo</div>
+        <div className='font-Druk text-5xl tracking-wide uppercase'><span>Fitness</span><span className='text-primary'>Hub</span></div>
     )
 }
