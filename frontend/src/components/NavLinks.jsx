207bed9e0a5472127257f55e5af2b72621615214
Carlos <carlosgilino@gmail.com>
2025-08-12 22:48:09 +0200

    create parent component for NavLinks

---
+++ frontend/src/components/NavLinks.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const navigationLinks = [
+    { id: 1, text: 'Home', url: '#' },
+    { id: 2, text: 'Plans', url: '#' },
+    { id: 3, text: 'Articles', url: '#' },
+    { id: 3, text: 'Suscribe', url: '#' },
+]
+
+export default function NavLinks({ direction, textSize }) {
+
+
+    const directionClasses = {
+        horizontal: 'flex-row',
+        vertical: 'flex-col'
+    }
+
+    const sizeClasses = {
+        base: 'text-base',
+        xl: 'text-5xl'
+    }
+
+    return (
+        <ul className={`flex ${directionClasses[direction]} gap-3 text-white justify-center`}>
+            {
+                navigationLinks.map((item, index) =>
+                    <li key={index}><a className={sizeClasses[textSize]} href={item.url}>{item.text}</a></li>
+                )
+            }
+        </ ul>
+    )
+}
