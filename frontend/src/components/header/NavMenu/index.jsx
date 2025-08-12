6675faf4a1951c6429ec4ebefbc28833f810bdf8
Carlos <carlosgilino@gmail.com>
2025-08-12 22:49:08 +0200

    use new NavLinks component on NavMenu

--- frontend/src/components/header/NavMenu/index.jsx
+++ frontend/src/components/header/NavMenu/index.jsx
@@ -2,26 +2,18 @@ import React from 'react'
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react'
 import MenuIcon from './MenuIcon'
 import CloseIcon from './CloseIcon'
-
-const menuItems = [
-  { id: 1, text: 'Home', url: '#' },
-  { id: 2, text: 'Plans', url: '#' },
-  { id: 3, text: 'Articles', url: '#' },
-  { id: 3, text: 'Suscribe', url: '#' },
-]
+import NavLinks from '../../NavLinks'
 
 export default function NavMenu() {
   return (
     <Disclosure>
       <DisclosureButton className="py-2"><MenuIcon /></DisclosureButton>
-      <DisclosurePanel transition className='absolute right-0 top-0 opacity-95 h-full w-3/4 sm:w-2/4 md:w-1/4 bg-gray-700 px-6 py-3 transition duration-200 ease-in data-closed:translate-x-full data-closed:opacity-0'>
+      <DisclosurePanel transition className='fixed z-1 right-0 top-0 opacity-95 h-full w-3/4 sm:w-2/4 md:w-1/4 bg-gray-700 px-6 py-3 transition duration-200 ease-in data-closed:translate-x-full data-closed:opacity-0'>
         <div className="flex w-full justify-end">
           <DisclosureButton className="py-2"><CloseIcon /></DisclosureButton>
         </div>
         <ul className='flex flex-col gap-4 p-3 text-white'>
-          {menuItems.map((item, index) =>
-            <li key={index}><a className='text-5xl' href={item.url}>{item.text}</a></li>
-          )}
+          <NavLinks direction='vertical' textSize='xl' />
         </ul>
       </DisclosurePanel>
     </Disclosure>
