d7760ba94bd902dea8368c610c50078b89529458
Carlos <carlosgilino@gmail.com>
2025-08-14 19:47:48 +0200

    improve hero bg

--- frontend/src/components/body/Hero/index.jsx
+++ frontend/src/components/body/Hero/index.jsx
@@ -4,9 +4,9 @@ import RightColumn from './RightColumn';
 
 export default function Hero() {
     return (
-        <div className='flex flex-col gap-6 lg:gap-0 lg:flex-row bg-gray-800 px-8 pt-8  md:px-16 md:pt-16'>
+        <div className='flex flex-col gap-6 lg:gap-0 lg:flex-row bg-gradient px-8 pt-8  md:px-16 md:pt-16 bg-gradient-to-r from-gray-800 to-gray-700'>
             <div className='lg:w-[60%] text-white'><LeftColumn /></div>
             <div className='lg:w-[40%] flex flex-col justify-end'><RightColumn /></div>
-        </div>
+        </div >
     )
 }
