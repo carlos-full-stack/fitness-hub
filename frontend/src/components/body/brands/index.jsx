d8b73cb31ca86cabb828a2f60f48db94f18bec19
Carlos <carlosgilino@gmail.com>
2025-08-14 11:10:21 +0200

    complete brand component. add brand imgs

--- frontend/src/components/body/brands/index.jsx
+++ frontend/src/components/body/brands/index.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
 const brands = [
-    { id: 'brand-1', name: 'x', url: '/img' },
-    { id: 'brand-2', name: 'y', url: '/img' },
-    { id: 'brand-3', name: 'z', url: '/img' },
-    { id: 'brand-4', name: 'n', url: '/img' },
+    { id: 'brand-1', name: 'ELEIKO', url: 'img/gym-brands/ELEIKO.png' },
+    { id: 'brand-2', name: 'gym80', url: 'img/gym-brands/gym80-1.png' },
+    { id: 'brand-3', name: 'HAMMER', url: 'img/gym-brands/HAMMER.png' },
+    { id: 'brand-4', name: 'Technogym', url: 'img/gym-brands/Technogym.png' },
 ]
 
 export default function Brands() {
     return (
         <div className='flex flex-row gap-4 justify-center bg-primary w-full p-4'>
-            <ul className='inline-flex flex-wrap gap-2'>
+            <ul className='inline-flex flex-wrap gap-x-12 gap-y-3 justify-center'>
                 {brands.map((brand) => (
                     <li key={brand.id}>
-                        <img src={brand.url} alt={brand.name} />
+                        <img src={brand.url} alt={brand.name} className='w-24 h-auto' />
                     </li>
 
                 ))}
