e47e768cfc35e13c7c938cdb7bd4803aa782385d
Carlos <carlosgilino@gmail.com>
2025-08-14 19:48:21 +0200

    fix icon components classes

--- frontend/src/components/header/NavMenu/MenuIcon.jsx
+++ frontend/src/components/header/NavMenu/MenuIcon.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function MenuIcon() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 12h16" /><path d="M4 18h16" /><path d="M4 6h16" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M4 12h16" /><path d="M4 18h16" /><path d="M4 6h16" /></svg>
     )
 }
