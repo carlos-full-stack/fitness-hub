265351b4125368c8180cfe0e7f9bdcff43c21870
Carlos <carlosgilino@gmail.com>
2025-08-21 13:30:21 +0200

    improve useApi to allow custom method and send data

--- frontend/src/hooks/useApi.jsx
+++ frontend/src/hooks/useApi.jsx
@@ -1,19 +1,19 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios';
 
-export default function useApi(url) {
+export default function useApi(url, method = 'get', body = null) {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
 
 
     useEffect(() => {
-        axios.get(url)
+        axios({ url, method, data: body })
             .then(response => setData(response.data))
             .catch(err => setError(err))
             .finally(() => setLoading(false))
 
-    }, [url])
+    }, [url, method, body])
 
 
 
