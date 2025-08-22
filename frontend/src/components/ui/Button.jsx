0147590318bb5f2d966d058b0f5556b603b79fa1
Carlos <carlosgilino@gmail.com>
2025-08-22 20:00:05 +0200

    add cursor pointer to Button component

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Button({ text, color = 'primary', type = 'button' }) {
 
-    const baseClasses = 'capitalize py-2 px-6 rounded-sm';
+    const baseClasses = 'capitalize cursor-pointer py-2 px-6 rounded-sm';
 
     const buttonColors = {
         primary: 'text-gray-800 bg-primary',
