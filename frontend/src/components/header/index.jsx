a893b044045bbb17e4b74709ae7702f152d4daaa
Carlos <carlosgilino@gmail.com>
2025-08-24 20:28:19 +0200

    add conditional rendering for Auth and NavMenu based on user state

--- frontend/src/components/header/index.jsx
+++ frontend/src/components/header/index.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
 import Logo from './Logo';
 import NavMenu from './NavMenu';
-import Button from '../ui/Button';
+import Auth from './Auth'
+import { useAuth } from '../../context/AuthContext';
 
 export default function Header() {
+
+    const { user } = useAuth();
+
     return (
         <div className='flex justify-between items-center px-8 md:px-14 h-20 bg-gray-600 text-white flex-shrink-0'>
             <Logo />
-            <NavMenu />
+            <div className='flex items-center gap-6'>
+                 {!user ? <Auth /> : <NavMenu />}
+            </div>
         </div>
     )
 }
