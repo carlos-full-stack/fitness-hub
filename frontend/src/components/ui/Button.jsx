b09ff964bcbf75048b16f420967f4fb1cb0ab7b5
Carlos <carlosgilino@gmail.com>
2025-08-24 20:12:05 +0200

    add onClick evento to props

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
 
-export default function Button({ text, color = 'primary', type = 'button' }) {
+export default function Button({ text, color = 'primary', type = 'button', onClick = () => {} }) {
 
     const baseClasses = 'capitalize cursor-pointer py-2 px-6 rounded-sm';
 
@@ -14,6 +13,6 @@ export default function Button({ text, color = 'primary', type = 'button' }) {
     const variantClasses = buttonColors[color];
 
     return (
-        <button type={type} className={`${baseClasses} ${variantClasses}`}> {text}</ button >
+        <button type={type} className={`${baseClasses} ${variantClasses}`} onClick={onClick}> {text}</ button >
     )
 }
