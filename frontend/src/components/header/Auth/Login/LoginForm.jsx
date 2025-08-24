cd63fe4b977397a3e59eb9bd4a3be23b8f45235c
Carlos <carlosgilino@gmail.com>
2025-08-24 20:11:28 +0200

    add labels to form, change login timeout

--- frontend/src/components/header/Auth/Login/LoginForm.jsx
+++ frontend/src/components/header/Auth/Login/LoginForm.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import Button from '../../../ui/Button';
 import axios from 'axios';
+import { useAuth } from '../../../../context/AuthContext';
 
 
 export default function LoginForm({ loginFormFields, onLoginResult = 'initial', onLoginSuccess }) {
     
+    const { login } = useAuth();
+
     const [formValues, setFormValues] = useState({});
     const [fieldErrors, setFieldErrors] = useState({});
     
@@ -21,6 +24,11 @@ export default function LoginForm({ loginFormFields, onLoginResult = 'initial', 
             if (response.data && response.data.token) {
                 onLoginResult('success');
                 onLoginSuccess();
+
+            setTimeout(() => {
+                login(response.data.user);
+            }, 2000);
+
             }
 
         } catch (error) {
@@ -37,15 +45,16 @@ export default function LoginForm({ loginFormFields, onLoginResult = 'initial', 
     return (
         <form onSubmit={handleSubmit} className='flex flex-col justify-center gap-3'>
             {loginFormFields.map((field, index) => (
-                <div key={index} className=' flex flex-col w-full md:size-auto'>
-                    <label htmlFor={field.name}></label>
-                    <input
-                        type={field.type}
-                        name={field.name}
-                        placeholder={field.placeholder}
-                        value={formValues[field.name] || ''}
-                        onChange={handleImputChange}
-                        className={`w-full md:size-auto border ${fieldErrors[field.name] ? 'border-red-500' : 'border-gray-800'} py-2 pr-3 pl-2 focus:outline-none`} />
+                <div key={index} className='flex flex-col w-full md:size-auto'>
+                    <label htmlFor={field.id}>{field.label}</label>
+                        <input
+                            id={field.id}
+                            type={field.type}
+                            name={field.name}
+                            placeholder={field.placeholder}
+                            value={formValues[field.name] || ''}
+                            onChange={handleImputChange}
+                            className={`w-full md:size-auto border ${fieldErrors[field.name] ? 'border-red-500' : 'border-gray-800'} py-2 pr-3 pl-2 focus:outline-none`} />
                     <span className="text-sm pt-2">{fieldErrors[field.name] && fieldErrors[field.name][0]}</span>
                 </div>
             ))}
