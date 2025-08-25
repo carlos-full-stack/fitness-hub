45cd470cba1486d65cbcdf529f6c1134ed4bd948
Carlos <carlosgilino@gmail.com>
2025-08-25 16:35:21 +0200

    change NavLinks props

--- frontend/src/components/header/NavMenu/index.jsx
+++ frontend/src/components/header/NavMenu/index.jsx
@@ -11,13 +11,11 @@ export default function NavMenu() {
   return (
     <Disclosure>
       <DisclosureButton className="flex items-center gap-2 py-2 text-2xl capitalize cursor-pointer"><UserIcon />{ user && user }</DisclosureButton>
-      <DisclosurePanel transition className='fixed z-1 right-0 top-0 opacity-95 h-full w-3/4 sm:w-2/4 md:w-1/4 bg-gray-700 px-6 py-3 transition duration-200 ease-in data-closed:translate-x-full data-closed:opacity-0'>
+      <DisclosurePanel transition className='fixed z-1 right-0 top-0 border-l border-l-gray-500 opacity-95 h-full w-3/4 sm:w-2/4 md:w-1/4 bg-gray-700 px-6 py-3 transition duration-200 ease-in data-closed:translate-x-full data-closed:opacity-0'>
         <div className="flex w-full justify-end">
           <DisclosureButton className="py-2 cursor-pointer"><CloseIcon /></DisclosureButton>
         </div>
-        <ul className='flex flex-col gap-4 p-3 text-white'>
-          <NavLinks direction='vertical' textSize='xl' />
-        </ul>
+          <NavLinks type='user' />
       </DisclosurePanel>
     </Disclosure>
   )
