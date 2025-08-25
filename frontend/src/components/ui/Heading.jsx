f70329b69f417bb7ec33e2f1591ede475bd1d141
Carlos <carlosgilino@gmail.com>
2025-08-25 16:36:29 +0200

    add id prop to allow internal links

--- frontend/src/components/ui/Heading.jsx
+++ frontend/src/components/ui/Heading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 
 
-export default function Heading({ level = 'h1', headingText, eyebrowText, subHeadingText = '', textAlign = 'left' }) {
+export default function Heading({ level = 'h1', id = '', headingText, eyebrowText, subHeadingText = '', textAlign = 'left' }) {
 
 
     const headingAlign = textAlign === 'left'
@@ -11,7 +11,7 @@ export default function Heading({ level = 'h1', headingText, eyebrowText, subHea
 
     return (
 
-        <div className={`${headingAlign} flex flex-col text-white`}>
+        <div className={`${headingAlign} flex flex-col text-white`} id={id}>
             {eyebrowText && <span className='text-primary text-sm py-2'>{eyebrowText}</span>}
             {level === 'h1' ? <h1 className='text-5xl md:text-8xl lg:text-6xl xl:text-8xl uppercase font-Druk tracking-wider leading-10 md:leading-20 lg:leading-12 xl:leading-20'> {headingText}</h1> : <h2 className='text-2xl md:text-5xl uppercase'> {headingText}</h2>}
             {subHeadingText && <p className='pt-6'>{subHeadingText}</p>}
