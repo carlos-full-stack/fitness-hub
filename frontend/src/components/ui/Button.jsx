77bbab2d2509e4607b5115ca67088724e5058645
Carlos <carlosgilino@gmail.com>
2025-08-25 17:19:34 +0200

    add hover color to Button

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -4,10 +4,10 @@ export default function Button({ text, color = 'primary', type = 'button', onCli
     const baseClasses = 'capitalize cursor-pointer py-2 px-6 rounded-sm';
 
     const buttonColors = {
-        primary: 'text-gray-800 bg-primary',
-        light: 'text-white bg-gray-700',
-        dark: 'text-white bg-gray-800',
-        transparent: 'text-white bg-transparent border border-white'
+        primary: 'text-gray-800 bg-primary hover:bg-primary-hover ',
+        light: 'text-white bg-gray-700 hover:bg-gray-700',
+        dark: 'text-white bg-gray-800 hover:bg-gray-900',
+        transparent: 'text-white bg-transparent border border-white hover:border-primary'
     }
 
     const variantClasses = buttonColors[color];
