c7b061cbdbcb3203aa4567c81487026bf1bc71b2
Carlos <carlosgilino@gmail.com>
2025-08-25 16:34:36 +0200

    add NavLinksType prop to apply different links and styles on NavLinks

--- frontend/src/components/NavLinks.jsx
+++ frontend/src/components/NavLinks.jsx
@@ -1,31 +1,73 @@
-import React from 'react'
+import { useNavigate, Link } from 'react-router-dom';
+import SquareArrowOutIcon from './ui/SquareArrowOutIcon';
+import { useAuth } from '../context/AuthContext';
 
-const navigationLinks = [
-    { id: 1, text: 'Home', url: '#' },
-    { id: 2, text: 'Plans', url: '#' },
-    { id: 3, text: 'Articles', url: '#' },
-    { id: 3, text: 'Suscribe', url: '#' },
+
+const pageLinks = [
+    { id: 1, text: 'Home', url: '#home' },
+    { id: 2, text: 'Who we are', url: '#who' },
+    { id: 3, text: 'Plans', url: '#pricing' },
+    { id: 4, text: 'Articles', url: '#articles' },
 ]
 
-export default function NavLinks({ direction, textSize }) {
+const userLinks = [
+    
+    { id: 1, text: 'Home', url: '/' },
+    { id: 2, text: 'Dashboard', url: '/dashboard' },
+    { id: 3, text: 'Logout', url: '/logout' },
+]
 
+export default function NavLinks({ type='page' }) {
+    
+    const { logout } = useAuth();
+    const { navigate } = useNavigate();
 
-    const directionClasses = {
-        horizontal: 'flex-col lg:flex-row',
-        vertical: 'flex-col'
+    const NavLinksType = 
+    {
+        user: { 
+            id: 1,
+            name: 'user',
+            links: userLinks,
+            style: 'flex-col h-full text-4xl gap-3'
+         },
+
+        page: { 
+         id: 2,
+         name: 'page',
+         links: pageLinks,
+         style: 'justify-center text-base gap-3'
+         }
+
     }
 
-    const sizeClasses = {
-        base: 'text-base',
-        xl: 'text-5xl'
-    }
+    const navigationLinks = NavLinksType[type];
+
+        const handleLogout = () => {
+            logout();
+            navigate('/');
+}
+    
+
 
     return (
-        <ul className={`flex ${directionClasses[direction]} gap-3 text-white justify-center`}>
+        <ul className={`flex ${navigationLinks['style']} text-white`}>
             {
-                navigationLinks.map((item, index) =>
-                    <li key={index}><a className={sizeClasses[textSize]} href={item.url}>{item.text}</a></li>
-                )
+                navigationLinks['links'].map((item, index) =>
+                    item.text === 'Logout'
+                       ? (
+                        <>
+
+                            <li onClick={handleLogout} className='flex items-center gap-3 absolute bottom-10' key={index}>
+                            <span className='cursor-pointer'>{item.text}</span><SquareArrowOutIcon />
+                            </li>
+                        </>
+                        )
+                       : (
+                            <li key={index}>
+                                <Link to={item.url}>{item.text}</Link>
+                            </li>
+                        )
+                    )    
             }
         </ ul>
     )
