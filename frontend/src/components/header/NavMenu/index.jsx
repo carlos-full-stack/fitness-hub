a008c730a779929df5d0d13d2989a9051d5d7127
Carlos <carlosgilino@gmail.com>
2025-08-25 17:19:55 +0200

    add hover color to user text

--- frontend/src/components/header/NavMenu/index.jsx
+++ frontend/src/components/header/NavMenu/index.jsx
@@ -10,7 +10,7 @@ export default function NavMenu() {
 
   return (
     <Disclosure>
-      <DisclosureButton className="flex items-center gap-2 py-2 text-2xl capitalize cursor-pointer"><UserIcon />{ user && user }</DisclosureButton>
+      <DisclosureButton className="flex items-center gap-2 py-2 text-2xl hover:text-gray-200 capitalize cursor-pointer"><UserIcon />{ user && user }</DisclosureButton>
       <DisclosurePanel transition className='fixed z-1 right-0 top-0 border-l border-l-gray-500 opacity-95 h-full w-3/4 sm:w-2/4 md:w-1/4 bg-gray-700 px-6 py-3 transition duration-200 ease-in data-closed:translate-x-full data-closed:opacity-0'>
         <div className="flex w-full justify-end">
           <DisclosureButton className="py-2 cursor-pointer"><CloseIcon /></DisclosureButton>
