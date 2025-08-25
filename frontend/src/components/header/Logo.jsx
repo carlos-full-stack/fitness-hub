2b1cc7ff2da748bf11b2231fa70e8ea34c2f892f
Carlos <carlosgilino@gmail.com>
2025-08-25 17:18:15 +0200

    Add link to Logo and improve code indentation

--- frontend/src/components/header/Logo.jsx
+++ frontend/src/components/header/Logo.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Logo() {
     return (
-        <div className='font-Druk text-5xl text-center md:text-start tracking-wide uppercase'><span className='text-white'>Fitness</span><span className='text-primary'>Hub</span></div>
+        <div className='font-Druk text-5xl text-center md:text-start tracking-wide uppercase'>
+            <Link to='/'>
+                <span className='text-white'>Fitness</span><span className='text-primary'>Hub</span>
+            </Link>
+        </div>
     )
 }
