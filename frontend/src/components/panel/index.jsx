69f0e30037df9eb9da65d92a66556555a513a856
Carlos <carlosgilino@gmail.com>
2025-08-26 16:38:15 +0200

    complete dashboard panel

--- frontend/src/components/panel/index.jsx
+++ frontend/src/components/panel/index.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import CardGallery from "../ui/Card/CardGallery";
+import Card from "../ui/Card";
+import Section from "../ui/Section";
+import Heading from "../ui/Heading";
+
+const dashBoardCards = [
+  { id: 1, title: "BMI" },
+  { id: 2, title: "Progress" },
+  { id: 3, title: "Calories" },
+  { id: 4, title: "Kms" },
+];
 
 export default function Panel() {
-    return (
-        <div>Panel del dashboard</div>
-    )
+  return (
+    <Section>
+      <div className="flex flex-col lg:flex-row gap-10">
+        <div className="flex-2/3">
+          <Heading level="h3" headingText="Dasboard" />
+          <CardGallery type="stats" cards={dashBoardCards} />
+        </div>
+        <div className="flex-1/3 bg-gray-500">Sidebar</div>
+      </div>
+    </Section>
+  );
 }
