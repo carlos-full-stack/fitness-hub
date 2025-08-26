7903064166748844ecd30ab5e5efea1ac9fc8507
Carlos <carlosgilino@gmail.com>
2025-08-26 15:52:04 +0200

    add stats cardType. code clean up

--- frontend/src/components/ui/Card/CardGallery.jsx
+++ frontend/src/components/ui/Card/CardGallery.jsx
@@ -1,34 +1,32 @@
-import React from 'react'
-import CardWithImage from './CardWithImage'
-import Card from './'
-import CardWithDescription from './CardWithDescription/'
-import CardWithPrice from './CardWithPrice'
-import CardWithRating from './CardWithRating'
+import React from "react";
+import CardWithImage from "./CardWithImage";
+import Card from "./";
+import CardWithDescription from "./CardWithDescription/";
+import CardWithPrice from "./CardWithPrice";
+import CardWithRating from "./CardWithRating";
+import CardWithStats from "./CardWithStats";
 
 const cardTypes = {
-    'image': CardWithImage,
-    'description': CardWithDescription,
-    'price': CardWithPrice,
-    'rating': CardWithRating,
-    default: Card
-
+  image: CardWithImage,
+  description: CardWithDescription,
+  price: CardWithPrice,
+  rating: CardWithRating,
+  stats: CardWithStats,
+  default: Card,
 };
 
-
 export default function CardGallery({ type, cards }) {
+  const CardComponent = cardTypes[type] || cardTypes.default;
 
-    const CardComponent = cardTypes[type] || cardTypes.default;
-
-    return (
-
-        <div className='w-full py-8 md:py-10'>
-            <ul className='flex flex-col lg:flex-row gap-6 w-full justify-center'>
-                {cards.map((card, index) => (
-                    <li className='flex-1 gap-6' key={index}>
-                        <CardComponent {...card} />
-                    </li>
-                ))}
-            </ul>
-        </div >
-    )
+  return (
+    <div className="w-full py-8 md:py-10">
+      <ul className="flex flex-col lg:flex-row gap-6 w-full justify-center">
+        {cards.map((card, index) => (
+          <li className="flex-1 gap-6" key={index}>
+            <CardComponent {...card} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
