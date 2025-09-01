7326d9a6f6b41468e06a4b6206fd5927508969da
Carlos <carlosgilino@gmail.com>
2025-09-01 23:03:55 +0200

    add user login responsibility to AuthProvider

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,26 +1,59 @@
+import axios from "axios";
 import { createContext, useState, useContext } from "react";
 
 const AuthContext = createContext({
-    user: null,
-    login: () => { },
-    logout: () => { }
+  user: null,
+  setUser: () => {},
+  login: () => {},
+  logout: () => {},
+  loading: false,
 });
 
 export default function AuthProvider({ children }) {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    const login = (userData) => setUser(userData);
-    const logout = () => setUser(null)
+  const login = async (credentials) => {
+    setLoading(true);
+    try {
+      const response = await axios.post("api/login", credentials);
+      if (response && response.data) {
+        const token = response.data.token;
+        localStorage.setItem("token", token);
 
+        const userResponse = await axios.get("api/user", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
 
+        return userResponse.data.user;
+      }
+    } catch (error) {
+      console.error("Error displaying data", {
+        status: error.response?.status,
+        data: error.response?.data,
+        message: error.message,
+      });
 
-    return (
-        <AuthContext.Provider value={{ user, login, logout }}>
-            {children}
-        </AuthContext.Provider>
-    )
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, login, logout, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 export function useAuth() {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 }
