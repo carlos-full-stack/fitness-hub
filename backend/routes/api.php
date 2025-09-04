fc8536f8aff5c08a9e700eb075d98f475cdc7119
Carlos <carlosgilino@gmail.com>
2025-09-04 20:14:01 +0200

    add PlanController and UserController routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,12 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PlanController;
+use App\Http\Controllers\UserController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('plans', [PlanController::class, 'showPlans']);
 
 // Protected Routes
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [UserController::class, 'showUser']);
+    Route::post('update', [UserController::class, 'update']);
     Route::post('logout', [AuthController::class, 'logout']);
 });
