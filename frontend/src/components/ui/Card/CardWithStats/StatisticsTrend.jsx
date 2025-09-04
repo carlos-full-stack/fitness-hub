995fae92ef32b3b1070f68024918cb0b33b5a79d
Carlos <carlosgilino@gmail.com>
2025-09-04 21:53:08 +0200

    create CardWithStats subcomponents

---
+++ frontend/src/components/ui/Card/CardWithStats/StatisticsTrend.jsx
@@ -0,0 +1,3 @@
+export default function StatisticsTrend() {
+  return <div>StatisticsTrend</div>;
+}
--- frontend/src/components/ui/Card/CardWithStats/index.jsx
+++ frontend/src/components/ui/Card/CardWithStats/index.jsx
@@ -1,9 +1,19 @@
 import Card from "..";
+import StatisticsBar from "./StatisticsBar";
+import StatisticsTrend from "./StatisticsTrend";
 
-export default function CardWithStats({ title }) {
+const cardTypes = {
+  bar: StatisticsBar,
+  trend: StatisticsTrend,
+};
+
+export default function CardWithStats({ title, type, value }) {
+  const Component = cardTypes[type];
+
   const children = (
     <div className="flex flex-col gap-2 p-6">
       <h3 className="text-xl">{title}</h3>
+      <Component value={value} />
     </div>
   );
 
