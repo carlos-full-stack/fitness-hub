48188815902ef866733698b2afd7d3083ee39389
Carlos <carlosgilino@gmail.com>
2025-09-12 12:06:04 +0200

    add workout routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PlanController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WorkoutController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -13,7 +13,15 @@ Route::get('plans', [PlanController::class, 'showPlans']);
 // Protected Routes
 
 Route::middleware('auth:sanctum')->group(function () {
+
     Route::get('user', [UserController::class, 'showUser']);
+    Route::get('user/bmi', [UserController::class, 'showUserBMI']);
     Route::post('update', [UserController::class, 'update']);
+
     Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::post('workout/store', [WorkoutController::class, 'store']);
+    Route::get('workout/getKmsProgress', [WorkoutController::class, 'getKmsProgress']);
+    Route::get('workout/getCaloriesProgress', [WorkoutController::class, 'getCaloriesProgress']);
+    Route::get('workout/getAttendanceProgress', [WorkoutController::class, 'getAttendanceProgress']);
 });
