431b0100d7184f5215b9733f02478c70607f8bac
Carlos <carlosgilino@gmail.com>
2025-09-15 17:06:42 +0200

    improve code indentation

--- frontend/src/components/ui/Card/CardWithPrice.jsx
+++ frontend/src/components/ui/Card/CardWithPrice.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import Card from '.'
-import Button from '../Button'
+import React from "react";
+import Card from ".";
+import Button from "../Button";
 
 export default function CardWithPrice({ price, title, description }) {
-
-    const children = <div className='flex flex-col justify-between gap-2 p-7 h-full'>
-        <div>
-            <span className='text-5xl'>{`${price}€`}</span>
-            <h3 className='text-xl pt-2'>{title}</h3>
-            <p className='py-4'>{description}</p>
-        </div>
-        <Button text='Discover more' />
+  const children = (
+    <div className="flex flex-col justify-between gap-2 p-7 h-full">
+      <div>
+        <span className="text-5xl">{`${price}€`}</span>
+        <h3 className="text-xl pt-2">{title}</h3>
+        <p className="py-4">{description}</p>
+      </div>
+      <Button text="Discover more" />
     </div>
+  );
 
-    return (
-        <Card children={children} />
-    )
+  return <Card children={children} />;
 }
