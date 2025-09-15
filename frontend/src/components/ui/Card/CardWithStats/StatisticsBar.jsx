4d8acd4196ca0e0400c932f9bfe4a39fd59fefaa
Carlos <carlosgilino@gmail.com>
2025-09-15 17:09:56 +0200

    complete StatisticsBar component

--- frontend/src/components/ui/Card/CardWithStats/StatisticsBar.jsx
+++ frontend/src/components/ui/Card/CardWithStats/StatisticsBar.jsx
@@ -1,32 +1,50 @@
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import {
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
 
-export default function StatisticsBar({ value }) {
+export default function StatisticsBar({ variant, value }) {
+  let barPercentage;
+  let pathColor;
+
+  if (variant === "BMI") {
+    const maxIMC = 40;
+    const percentage = (value.imc / maxIMC) * 100;
+    barPercentage = Math.round(percentage);
+
+    if (barPercentage <= 25) {
+      pathColor = "#4ade80";
+    } else if (barPercentage <= 50) {
+      pathColor = "#fde047";
+    } else if (barPercentage <= 75) {
+      pathColor = "#fbbf24";
+    } else {
+      pathColor = "#f87171";
+    }
+  } else {
+    barPercentage = value.attendance_progress;
+    pathColor = "#d8f900";
+  }
+
   return (
-    <CircularProgressbar
-      value={value}
-      text={`${value}%`}
-      styles={buildStyles({
-        // Rotation of path and trail, in number of turns (0-1)
-        rotation: 0.25,
-
-        // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-        strokeLinecap: "butt",
-
-        // Text size
-        textSize: "16px",
-
-        // How long animation takes to go from one percentage to another, in seconds
-        pathTransitionDuration: 0.5,
-
-        // Can specify path transition in more detail, or remove it entirely
-        // pathTransition: 'none',
-
-        // Colors
-        pathColor: `rgba(62, 152, 199, ${value / 100})`,
-        textColor: "#f88",
-        trailColor: "#d6d6d6",
-        backgroundColor: "#3e98c7",
-      })}
-    />
+    <div className="w-full flex flex-col items-center justify-center grow">
+      <div className="w-32">
+        <CircularProgressbarWithChildren
+          value={barPercentage}
+          strokeWidth={8}
+          styles={buildStyles({
+            rotation: 0,
+            strokeLinecap: "butt",
+            pathColor: `${pathColor}`,
+            trailColor: "#FFFFFF",
+          })}
+        >
+          <div className="flex flex-col items-center justify-center h-full text-white">
+            <span className="text-sm">{value.category}</span>
+          </div>
+        </CircularProgressbarWithChildren>
+      </div>
+    </div>
   );
 }
+//
