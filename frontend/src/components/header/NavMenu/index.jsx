79b1c4bc5b34797b61697d3e26d3412eeea4587d
Carlos <carlosgilino@gmail.com>
2025-09-22 23:00:22 +0200

    change Disclosure to Dialog

--- frontend/src/components/header/NavMenu/index.jsx
+++ frontend/src/components/header/NavMenu/index.jsx
@@ -1,34 +1,39 @@
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-} from "@headlessui/react";
+import { Dialog, DialogPanel } from "@headlessui/react";
 import CloseIcon from "./CloseIcon";
 import NavLinks from "../../NavLinks";
 import { useAuth } from "../../../context/AuthContext";
 import UserAvatar from "../Auth/UserAvatar";
+import { useState } from "react";
 
 export default function NavMenu() {
   const { user } = useAuth();
-
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <Disclosure>
-      <DisclosureButton className="flex items-center gap-2 py-2 text-2xl hover:text-gray-200 capitalize cursor-pointer">
-        <UserAvatar size={30} />
-        {user && user.name}
-      </DisclosureButton>
-      <DisclosurePanel
-        transition
-        className="fixed z-1 right-0 top-0 border-l border-l-gray-500 opacity-95 h-full w-3/4 sm:w-2/4 md:w-1/4 bg-gray-700 px-6 py-3 transition duration-200 ease-in data-closed:translate-x-full data-closed:opacity-0"
+    <>
+      <button
+        className="flex items-center gap-3 py-2 text-2xl hover:text-gray-200 capitalize cursor-pointer"
+        onClick={() => setIsOpen(true)}
       >
-        <div className="flex w-full justify-end">
-          <DisclosureButton className="py-2 cursor-pointer">
-            <CloseIcon />
-          </DisclosureButton>
-        </div>
-        <NavLinks type="user" />
-      </DisclosurePanel>
-    </Disclosure>
+        <UserAvatar size={30} />
+        {user && <span className="text-base md:text-lg">{user.name}</span>}
+      </button>
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+        <DialogPanel
+          transition
+          className="fixed z-1 right-0 top-0 border-l border-l-gray-500 opacity-95 h-full w-2/4 lg:w-1/4 bg-gray-700 px-6 py-3 transition duration-200 ease-in data-closed:translate-x-full data-closed:opacity-0"
+        >
+          <div className="flex w-full justify-end">
+            <button
+              className="py-2 cursor-pointer"
+              onClick={() => setIsOpen(false)}
+            >
+              <CloseIcon />
+            </button>
+          </div>
+          <NavLinks type="user" />
+        </DialogPanel>
+      </Dialog>
+    </>
   );
 }
