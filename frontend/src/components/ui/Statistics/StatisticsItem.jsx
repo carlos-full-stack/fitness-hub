526644e808557c887a0d8617390f8e58e5dc7868
Carlos <carlosgilino@gmail.com>
2025-09-22 23:13:39 +0200

    improve style

--- frontend/src/components/ui/Statistics/StatisticsItem.jsx
+++ frontend/src/components/ui/Statistics/StatisticsItem.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-
 export default function StatisticsItem({ number, text }) {
-    return (
-        <div className='flex flex-col gap-2'>
-            {number && <span className='text-3xl md:text-5xl lg:text-3xl xl:text-5xl text-stroke'>{number}</span>}
-            {text && <span className='text-lg capitalize'>{text}</span>}
-        </div>
-    )
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex">
+        {number && (
+          <span className="text-3xl md:text-5xl lg:text-3xl xl:text-5xl text-stroke mr-2">
+            {number}
+          </span>
+        )}
+        {text !== "years" ? (
+          <span className="text-xl">K</span>
+        ) : (
+          <span className="text-2xl">+</span>
+        )}
+      </div>
+      {text && <span className="text-xl capitalize">{text}</span>}
+    </div>
+  );
 }
