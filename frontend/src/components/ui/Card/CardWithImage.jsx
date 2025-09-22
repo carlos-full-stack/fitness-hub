9e95d46536cad7bdcb3850858088393df9ef6ed7
Carlos <carlosgilino@gmail.com>
2025-09-22 23:03:39 +0200

    code cleanup and indentation

--- frontend/src/components/ui/Card/CardWithImage.jsx
+++ frontend/src/components/ui/Card/CardWithImage.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import Card from './index'
+import Card from "./index";
 
-export default function CardWithImage({ variant = 'image', bgUrl = 'none', text = '' }) {
+export default function CardWithImage({
+  variant = "image",
+  bgUrl = "none",
+  text = "",
+}) {
+  const children = (
+    <>
+      <div className="absolute left-0 bottom-0 w-32 h-32 bg-primary blur-3xl"></div>
+      <span className="absolute left-[20px] bottom-[20px] font-Druk uppercase text-3xl text-white tracking-wider text-shadow-lg">
+        {text}
+      </span>
+    </>
+  );
 
-    const children = <span className='absolute left-[20px] bottom-[20px] font-Druk uppercase text-3xl text-white tracking-wider text-shadow-lg'>{text}</span>
-
-    return (
-        <Card variant='image' bgUrl={bgUrl} children={children} />
-    )
+  return <Card variant="image" bgUrl={bgUrl} children={children} />;
 }
