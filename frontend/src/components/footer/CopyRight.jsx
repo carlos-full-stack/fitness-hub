2a49433f10f5cf839e36e66c3e370cc315f8c747
Carlos <carlosgilino@gmail.com>
2025-09-22 22:58:21 +0200

    code cleanup and indentation

--- frontend/src/components/footer/CopyRight.jsx
+++ frontend/src/components/footer/CopyRight.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
-const copyRightText = <span>FitnessHub 2025 - <a href="https://github.com/carlos-full-stack" target='_blank'>Carlos</a></span>
-
 export default function CopyRight({ className }) {
-    return (
-        <div className={`${className} flex flex-col justify-center border-t border-b-slate-400 text-center text-white`}>{copyRightText}</div>
-    )
+  return (
+    <div
+      className={`${className} flex flex-col justify-center border-t border-b-slate-400 text-center text-white`}
+    >
+      <span>
+        FitnessHub 2025 -{" "}
+        <a href="https://www.carlosfullstack.dev/" target="_blank">
+          Carlos Full Stack
+        </a>
+      </span>
+    </div>
+  );
 }
